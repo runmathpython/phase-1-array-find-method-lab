86c954c4fd0ecb41d8ced032daa8496ac371a8b4
Seong Kim <runmath@gmail.com>
2023-03-21 12:23:33 -0600

    complete Array Find Method Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record) {
+    function didWin(element) {
+        return (element.result === "W")
+    }
+    let result = record.find(didWin)
+    if ( result === undefined){
+        return result
+    } else {
+        return result.year
+    }
+}
